a6857213cee3f5104b39ed7f89a4e83d0899d9aa
Khinshan Khan <khanshan501@gmail.com>
2019-11-28 01:29:32 -0500

    easter egg now on homepage. noticed sakura is extremely slow

--- assets/easter/js/main.js
+++ assets/easter/js/main.js
@@ -1,39 +1,29 @@
 playAudio = function(file){
-    var a = document.createElement('audio');
-    if(!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
+  var a = document.createElement('audio');
+  if(!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
   	var sound = new Audio(file);
   	sound.play();
-    }
+  }
 };
 
 var egg = new Egg();
 
 egg.addCode("up,up,down,down,left,right,left,right,b,a", function() {
-    var life = jQuery('<img src="/assets/easter/img/1up.png""><img>');
-    life.appendTo('body').css({position: "fixed", top: "200px", right: "100px", "z-index": 99999999});
-    life.animate({top: '50px', opacity: 0}, {
+  var life = jQuery('<img src="/assets/easter/img/1up.png""><img>');
+  life.appendTo('body').css({position: "fixed", top: "200px", right: "100px", "z-index": 99999999});
+  life.animate({top: '50px', opacity: 0}, {
 	duration: 1000,
 	complete: function(){
-            jQuery(this).remove();
+      jQuery(this).remove();
 	}
-    });
-    playAudio('/assets/easter/audio/1up.wav');
+  });
+  playAudio('/assets/easter/audio/1up.wav');
 });
 
 
 egg.addCode("esc,esc", function() {
-    /*
-      var div = document.createElement('div');
-      div.id = 'acidburn';
-      div.innerHTML = "WOO!<br /> YOU'VE DISCOVERED AN EASTER EGG<br /> TRY KONAMI CODE";
-      document.body.appendChild(div);
-
-      window.setTimeout(function() {
-      document.body.removeChild(div);
-      }, 10000);
-    */
   var all = document.getElementById("quote").innerHTML;
-  document.getElementById("quote").innerHTML = "<h2>WOO!<br /><br /> YOU'VE DISCOVERED AN EASTER EGG<br /><br /> TRY KONAMI CODE<h2>";
+  document.getElementById("quote").innerHTML = "WOO!<br />YOU'VE DISCOVERED AN EASTER EGG<br /> TRY KONAMI CODE";
   document.getElementById("quote").style.textAlign = "center";
   window.setTimeout(function() {
 	document.getElementById("quote").innerHTML = all;
